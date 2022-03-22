be41c2adff920963f8156da853e8a8a1b77555f1
YehonatanKadosh <jacojacoj3@gmail.com>
2022-03-22 19:56:35 +0200

    added IsOptional to all properties

--- src/apatrments/dto/update-apatrment.dto.ts
+++ src/apatrments/dto/update-apatrment.dto.ts
@@ -1,4 +1,43 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 import { CreateApatrmentDto } from './create-apatrment.dto';
 
-export class UpdateApatrmentDto extends PartialType(CreateApatrmentDto) {}
+export class UpdateApatrmentDto extends PartialType(CreateApatrmentDto) {
+  @IsNumber()
+  @ApiProperty({ description: 'The number of an apartment', required: true })
+  @IsOptional()
+  AppartmentNumber: number;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ description: 'The city of an apartment', required: true })
+  @IsOptional()
+  City: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ description: 'The street of an apartment', required: true })
+  @IsOptional()
+  Street: string;
+
+  @IsNumber()
+  @ApiProperty({
+    description: 'The building number of an apartment',
+    required: true,
+  })
+  @IsOptional()
+  BuildingNumber: number;
+
+  @IsNumber()
+  @ApiProperty({
+    description: 'The number of rooms of an apartment',
+    required: true,
+  })
+  @IsOptional()
+  NumberOfRooms: number;
+
+  @IsNumber()
+  @ApiProperty({ description: 'The rent of an apartment', required: true })
+  @IsOptional()
+  Rent: number;
+}
--- src/contacts/dto/update-contact.dto.ts
+++ src/contacts/dto/update-contact.dto.ts
@@ -1,4 +1,58 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
+import {
+  IsArray,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsPhoneNumber,
+  IsString,
+  Matches,
+} from 'class-validator';
+import { Pet, phoneNumberPattern } from '../schemas/contact.schema';
 import { CreateContactDto } from './create-contact.dto';
 
-export class UpdateContactDto extends PartialType(CreateContactDto) {}
+export class UpdateContactDto extends PartialType(CreateContactDto) {
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  @ApiProperty({ description: 'The first name of a contact' })
+  FirstName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ description: 'The last name of a contact' })
+  @IsOptional()
+  LastName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsPhoneNumber('IL')
+  @Matches(phoneNumberPattern)
+  @IsOptional()
+  @ApiProperty({
+    description: 'The phone number of a contact',
+    required: true,
+  })
+  PhoneNumber: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @IsOptional()
+  @ApiProperty({
+    description: 'The appartment number of a contact',
+    required: true,
+  })
+  AppartmentNumber: number;
+
+  @IsArray()
+  @IsNotEmpty()
+  @IsOptional()
+  @ApiProperty({
+    description: 'The pets list of a contact',
+    type: [Pet],
+    enum: Pet,
+    default: [],
+    isArray: true,
+  })
+  pets: Pet[] = [];
+}
