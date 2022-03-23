527035ade09cdb154eb3bf19aa5ae90bdc8833cf
YehonatanKadosh <jacojacoj3@gmail.com>
2022-03-23 18:31:21 +0200

    added ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,19 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ContactsModule } from './contacts/contacts.module';
 import { ApatrmentsModule } from './apatrments/apatrments.module';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/home-residence'),
+    ConfigModule.forRoot(),
     ContactsModule,
     ApatrmentsModule,
     AuthModule,
+    UsersModule,
+    MongooseModule.forRoot(process.env.MONGO_KEY),
   ],
+  controllers: [AppController],
 })
 export class AppModule {}
