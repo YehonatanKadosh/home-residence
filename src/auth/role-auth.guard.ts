e6112fe5949fbb44c07e77b37063846c273a5114
YehonatanKadosh <jacojacoj3@gmail.com>
2022-03-24 14:11:02 +0200

    init

---
+++ src/auth/role-auth.guard.ts
@@ -0,0 +1,23 @@
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { AuthGuard } from '@nestjs/passport';
+import { Roles, User } from 'src/users/schemas/user.schema';
+
+@Injectable()
+export class RolesGuard extends AuthGuard('admin') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> {
+    const requiredRoles = this.reflector.get<Roles[]>(
+      'roles',
+      context.getHandler(),
+    );
+    if (!requiredRoles) return true;
+
+    const request = context.switchToHttp().getRequest();
+    const user: Partial<User> = request.user;
+
+    return requiredRoles.includes(user.role);
+  }
+}
