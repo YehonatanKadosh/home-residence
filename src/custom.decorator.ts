b106c8d485f56fd22d7b742a6555a65921ae11e0
YehonatanKadosh <jacojacoj3@gmail.com>
2022-03-24 17:27:16 +0200

    added SkipJWTAuth

--- src/custom.decorator.ts
+++ src/custom.decorator.ts
@@ -1,4 +1,7 @@
 import { SetMetadata } from '@nestjs/common';
 import { Roles as userRoles } from 'src/users/schemas/user.schema';
 
+export const IS_PUBLIC_KEY = 'noJWTRequired';
+export const SkipJWTAuth = () => SetMetadata(IS_PUBLIC_KEY, true);
+
 export const Roles = (...roles: userRoles[]) => SetMetadata('roles', roles);
