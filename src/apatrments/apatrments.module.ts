2671931b42db8cb9030e99bbd01358a3bfe4290c
YehonatanKadosh <jacojacoj3@gmail.com>
2022-03-24 17:27:57 +0200

    imports ContactsModule

--- src/apatrments/apatrments.module.ts
+++ src/apatrments/apatrments.module.ts
@@ -3,14 +3,17 @@ import { ApatrmentsService } from './apatrments.service';
 import { ApatrmentsController } from './apatrments.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Apartment, ApartmentSchema } from './schemas/apatrment.schema';
+import { ContactsModule } from 'src/contacts/contacts.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Apartment.name, schema: ApartmentSchema },
     ]),
+    ContactsModule,
   ],
   controllers: [ApatrmentsController],
   providers: [ApatrmentsService],
+  exports: [ApatrmentsService],
 })
 export class ApatrmentsModule {}
