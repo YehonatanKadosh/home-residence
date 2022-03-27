71b4e5c4af43342550491199227a5ac35d977903
YehonatanKadosh <jacojacoj3@gmail.com>
2022-03-27 14:30:48 +0300

    renamed

--- src/apatrments/dto/building.dto.ts
+++ src/apatrments/dto/building.dto.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
-export class RazeBuildingRentDto {
+export class BuildingDto {
   @IsString()
   @IsNotEmpty()
   @ApiProperty({ description: "The building's city", required: true })
